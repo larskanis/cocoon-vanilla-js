f1609135c4230099fc0dfa4241d86046a6eedbfc
Lars Kanis <lars@greiz-reinsdorf.de>
2023-03-03 16:31:41 +0100

    I mean turbo:render

--- index.js
+++ index.js
@@ -181,5 +181,5 @@ const hideFields = () => {
 
 document.addEventListener('DOMContentLoaded', hideFields);
 document.addEventListener('turbolinks:load', hideFields);
-document.addEventListener('turbo:reload', hideFields);
+document.addEventListener('turbo:render', hideFields);
 document.addEventListener('page:load', hideFields);
